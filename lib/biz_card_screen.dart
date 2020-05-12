66c206dbb86ed59a474af3c21c96423c16205955
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-12 10:59:18 +0530

    third commit

--- lib/biz_card_screen.dart
+++ lib/biz_card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,8 +10,71 @@ class BizCard extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[],
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            _getCard(),
+            _getAvatar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      height: 250,
+      width: 350,
+      margin: EdgeInsets.all(50.0),
+      decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Amit Maurya',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'amitmaurya@gmail.com',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person_outline,color: Colors.white,),
+              Text("T:amit@twitter.com", style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(50.0),
+        ),
+        border: Border.all(color: Colors.white, width: 2),
+        image: DecorationImage(
+          image: NetworkImage('https://picsum.photos/300/300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
